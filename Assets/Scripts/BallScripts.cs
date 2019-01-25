7a21a950b5ea7aac329706180011b7ec58694429
pickupst <gd.okanuslu@gmail.com>
2019-01-25 14:58:54 +0300

    Add ball and moving bombs

---
+++ Assets/Scripts/BallScripts.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+public class BallScripts : MonoBehaviour
+{
+    public GameObject[] bombs;
+
+    int rowCount = 0;
+
+    public bool isAllowOneDown = false;
+
+    List<GameObject> leftBombs = new List<GameObject>();
+    List<GameObject> rightBombs = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        BuildBlocks();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void BuildBlocks()
+    {
+
+        float bombWidth = bombs[0].GetComponent<Renderer>().bounds.size.x;
+        float ballWidth = gameObject.GetComponent<Renderer>().bounds.size.x;
+
+        Vector3 upperCornet = new Vector3(Screen.width, Screen.height, 0);
+        Vector3 targetWidth = Camera.main.ScreenToWorldPoint(upperCornet);
+
+        rowCount = (int) (targetWidth.y / bombWidth);
+
+        for (int i = 0; i < rowCount; i++)
+        {
+
+            int randomBombIndex = UnityEngine.Random.Range(0, bombs.Length);
+
+            leftBombs.Add((GameObject) Instantiate(bombs[randomBombIndex], new Vector3(-targetWidth.x + (bombWidth/2), i * bombWidth, 0), Quaternion.identity));
+            rightBombs.Add(Instantiate(bombs[(randomBombIndex + 1 ) % bombs.Length], new Vector3(+targetWidth.x - (bombWidth / 2), i * bombWidth, 0), Quaternion.identity));
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        moveDownBombs();
+    }
+
+    private void moveDownBombs()
+    {
+
+        if (isAllowOneDown)
+        {
+
+            for (int i = rowCount - 1; i > 0; i--)
+            {
+
+                leftBombs[i].GetComponent<Rigidbody2D>().MovePosition(leftBombs[i].GetComponent<Rigidbody2D>().position + new Vector2(0, -5 * Time.deltaTime));
+                rightBombs[i].GetComponent<Rigidbody2D>().MovePosition(rightBombs[i].GetComponent<Rigidbody2D>().position + new Vector2(0, -5 * Time.deltaTime));
+            }
+
+        }
+
+    }
+}
